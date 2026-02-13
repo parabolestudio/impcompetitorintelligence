a0a6b80af2f5de1738960e167659074c82c3283d
Kristin Baumann <hello@kristin-baumann.com>
2026-02-13 11:58:01 +0100

    Added axis lines and labels

--- js/Vis1.js
+++ js/Vis1.js
@@ -1,6 +1,5 @@
 import { html, useEffect, useState } from "./preact-htm.js";
-import Diamond from "./Diamond.js";
-import { Company, CompanyWithDiamond } from "./Company.js";
+import { CompanyWithDiamond } from "./Company.js";
 // import { REPO_BASE_URL } from "./helpers.js";
 
 export function Vis1() {
@@ -32,13 +31,21 @@ export function Vis1() {
   const visContainer = document.querySelector("#vis-1");
   const width =
     visContainer && visContainer.offsetWidth ? visContainer.offsetWidth : 600;
-  const height = 400;
+  const height1 = 150; // upper section with former company names
+  const height2 = 200; // middle section with lines connecting to current company names
+  const height3 = 1000; // lower section with current company names and logos
+  const height = height1 + height2 + height3;
   const margin = {
-    top: 20,
-    right: 20,
-    bottom: 20,
-    left: 20,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
   };
+  console.log("Vis 1 dimensions: ", {
+    width,
+    height,
+    margin,
+  });
 
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
@@ -62,11 +69,29 @@ export function Vis1() {
           fill="#f2f2f2"
           stroke="none"
         />
-        <g transform="translate(100, 100)">
+        <g>
+          <line
+            x1="0"
+            y1="${height1}"
+            x2="${width}"
+            y2="${height1}"
+            stroke="var(--color-vis-neutral-grey2)"
+          />
+          <text class="axis-text" y="${height1 - 5}">Former firms</text>
+          <line
+            x1="0"
+            y1="${height1 + height2}"
+            x2="${width}"
+            y2="${height1 + height2}"
+            stroke="var(--color-vis-neutral-grey2)"
+          />
+          <text class="axis-text" y="${height1 + height2 - 5}">New firms</text>
+        </g>
+        <g transform="translate(100, 600)">
           <${CompanyWithDiamond} name="IFM Investors" number=${14} />
           <circle cx="0" cy="0" r="5" fill="lightblue" />
         </g>
-        <g transform="translate(300, 100)">
+        <g transform="translate(300, 600)">
           <${CompanyWithDiamond}
             name="Ares Management Corporation"
             number=${8}

941f213f6c100a4e0b6229e7339ede6d627b5528
Kristin Baumann <hello@kristin-baumann.com>
2026-02-17 17:59:52 +0100

    Adjust code for embed

--- js/preact-htm.js
+++ js/preact-htm.js
@@ -5,3 +5,24 @@ import { useState, useEffect, useRef } from "https://esm.sh/preact/hooks";
 export const html = htm.bind(h);
 export const renderComponent = render;
 export { useState, useEffect, useRef };
+
+const Vis = async (props) => {
+  // console.log("Rendering Vis component with props:", props);
+  return html` <${props.component} ...${props} /> `;
+};
+
+export const renderVis = function (vis) {
+  const containerElement = document.getElementById(vis.id);
+  if (containerElement) {
+    // clear existing content before rendering
+    containerElement.innerHTML = "";
+
+    // wait for async Vis to resolve before rendering
+    (async () => {
+      const rendered = await Vis(vis);
+      renderComponent(rendered, containerElement);
+    })();
+  } else {
+    console.error(`Could not find container element for vis with id ${vis.id}`);
+  }
+};
--- script_vis2.js
+++ script_vis2.js
@@ -1,13 +1,5 @@
-console.log("Viz script loaded");
-
 import { renderVis } from "./js/preact-htm.js";
-import { Vis1 } from "./js/Vis1.js";
 import { Vis2 } from "./js/Vis2.js";
-
-renderVis({
-  id: "vis-1",
-  component: Vis1,
-});
 
 renderVis({
   id: "vis-2",

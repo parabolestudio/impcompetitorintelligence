25e0aeb20c22ec0807398a4bac33b83b97a4d990
Kristin Baumann <hello@kristin-baumann.com>
2026-03-12 13:31:33 +0100

    Added script entry points for modals

---
+++ script_vis1_modal.js
@@ -0,0 +1,7 @@
+import { renderVis } from "./js/lib.js";
+import { Vis1 } from "./js/Vis1.js";
+
+renderVis({
+  id: "vis-1-modal",
+  component: Vis1,
+});
---
+++ script_vis2_modal.js
@@ -0,0 +1,7 @@
+import { renderVis } from "./js/lib.js";
+import { Vis2 } from "./js/Vis2.js";
+
+renderVis({
+  id: "vis-2-modal",
+  component: Vis2,
+});

d780f772ec059731a9ba83acaba0e285a2d1da00
Kristin Baumann <hello@kristin-baumann.com>
2026-05-04 17:19:09 +0200

    Adjusted code overwrite

--- script.js
+++ script.js
@@ -13,14 +13,3 @@ renderVis({
   id: "vis-2",
   component: Vis2,
 });
-
-// with JavaScript detect a div with the min-height of 880px and the class bubble-element and "CustomElement" and adjust the inline style setting of height to 985px
-const bubbleElements = document.querySelectorAll(
-  ".bubble-element.CustomElement",
-);
-bubbleElements.forEach((element) => {
-  const minHeight = parseInt(window.getComputedStyle(element).minHeight);
-  if (minHeight === 880) {
-    element.style.height = "985px";
-  }
-});
